66527575db3af06b26fab8a7bf3b3eb578423e8e
jonathan450 <44813750+jonathan450@users.noreply.github.com>
2018-11-07 10:58:25 -0700

    Add files via upload

---
+++ guessmynumber3001.py
@@ -0,0 +1,29 @@
+
+
+
+import random
+
+secret = random.randint(1,99)
+
+guess = 0
+tries = 0
+print("Can you guess my number?")
+print("My number is an integer between 1 and 99")
+
+
+while guess != secret and tries <6:
+    print("What is your guess?")
+    guess = int(input())
+    if guess < secret :
+        print("Too Low!")
+    elif guess > secret :
+        print("Too High!")
+    tries = tries + 1
+
+
+if guess == secret :
+    print("You got it!")
+else :
+    print("No more guesses! Try again1")
+    print("the secret number is : ", secret)
+
